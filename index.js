a1aa9eaa2bedaade3660b71655d538d5d43878be
Azhara Kalbaeva <az.kalbaeva@gmail.com>
2021-06-08 14:45:19 +0600

    css changes

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
-function myFunction() {
-  document.getElementById("navbar__link").classList.toggle(".navbar__link");
+function clickMenu() {
+  document
+    .getElementById("navbar__link")
+    .classList.toggle("navbar__link-responsive");
   console.log("im clicked");
 }
-
-window.onclick = function (event) {
-  if (!event.target.matches(".navbar__icon")) {
-    var dropdowns = document.getElementsByClassName(".navbar__link");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains(".navbar__link ")) {
-        openDropdown.classList.remove(".navbar__link ");
-      }
-    }
-  }
-};
